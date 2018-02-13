f6f5fbea71ee58d3faeb9d55e70a2446e3cc9adb
Joseph <cutinoj@mail.gvsu.edu>
2018-02-13 12:53:50 -0500

    added shmkey

--- reader.c
+++ reader.c
@@ -8,11 +8,15 @@
 
 #define SIZE 4096
 
-char path = "/tmp";
-char id = 'S';
+char *path = "/tmp";
+int shmId;
+char *shmPtr;
 key_t semkey;
 
 void retrieveSmKey(void);
+void openSharedMemory(void);
+void closeSharedMemory(void);
+
 
 typedef struct commData{
 	char *msg;
@@ -47,4 +51,28 @@ void retrieveSmKey(void){
 		perror("IPC error: ftok"); 
 		exit(1);
 	}
+}
+
+void openSharedMemory(void){
+	if ((shmId =
+         shmget (IPC_PRIVATE, FOO,
+                 IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+        perror ("i can't get no..\n");
+        exit (1);
+    }
+    if ((shmPtr = shmat (shmId, 0, 0)) == (void *) -1) {
+        perror ("can't attach\n");
+        exit (1);
+    }
+}
+
+void closeSharedMemory(void){
+	if (shmdt (shmPtr) < 0) {
+        perror ("just can't let go\n");
+        exit (1);
+    }
+    if (shmctl (shmId, IPC_RMID, 0) < 0) {
+        perror ("can't deallocate\n");
+        exit (1);
+    }
 }
